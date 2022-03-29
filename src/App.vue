a6884e1a21312e43ae861f57e0ccb258a753908f
mwchoen <mwcheon@cudo.co.kr>
2022-03-30 08:24:53 +0900

    chart 예제 수정

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,33 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Chart } from "chart.js";
+import Chart from "chart.js/auto";
+import { ChartConfiguration } from "chart.js";
 
 export default Vue.extend({
   name: "App",
   mounted() {
+    const labels = ["January", "February", "March", "April", "May", "June"];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 10, 5, 2, 20, 30, 45],
+        },
+      ],
+    };
+    const config: ChartConfiguration = {
+      type: "line",
+      data,
+      options: {},
+    };
     const ctx = (
       document.getElementById("myChart") as HTMLCanvasElement
-    ).getContext("2d");
-    const cart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: ["January", "February", "April", "May", "June", "July"],
-        datasets: [
-          {
-            label: "My first dataset",
-            backgroundColor: "rgb(255,99,132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: [0, 10, 5, 2, 20, 30, 45],
-          },
-        ],
-      },
-      options: {},
-    });
+    ).getContext("2d") as CanvasRenderingContext2D;
+    let myChart = new Chart(ctx, config);
   },
 });
 </script>
